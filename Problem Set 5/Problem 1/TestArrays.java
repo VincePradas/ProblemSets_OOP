59fa9cbb78b1b63c5aac05c6e23c2375d13e830d
Vince Warren Pradas <pradasvince@gmail.com>
2025-03-12 19:12:34 +0800

    Problem Set 5

--- Problem Set 5/Problem 1/TestArrays.java
+++ Problem Set 5/Problem 1/TestArrays.java
@@ -12,7 +12,7 @@ public class TestArrays {
 
     public static void main(String[] args) {
         int[] array1 = { 2, 3, 5, 7, 11, 13, 17, 19 };
-        int[] array2 = new int[array1.length];
+        int[] array2 = array1;
 
         for(int i = 0; i<array1.length; i++)
         {
@@ -21,8 +21,33 @@ public class TestArrays {
             else
                 array2[i] = array1[i];
         }
+        
         printArray(array1);
         System.out.println();
         printArray(array2);
+        System.out.println();
+        
+        int[][] matrix = new int[5][];
+
+        for (int i = 0; i < matrix.length; i++) {
+            matrix[i] = new int[i];
+            for (int j = 0; j < matrix[i].length; j++) {
+                matrix[i][j] = i * j;
+            }
+        }
+
+        for (int i = 0; i < matrix.length; i++) {
+            System.out.print("matrix[" + i + "] is <");
+            for (int j = 0; j < matrix[i].length; j++) {
+                System.out.print(matrix[i][j]);
+                if (j < matrix[i].length - 1) {
+                    System.out.print(", ");
+                }
+            }
+            System.out.println(">");
+        }
+        
     }
+    
+    
 }
